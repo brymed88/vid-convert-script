6e906d6e0e90b144f890aaa83a0665000bbc7b65
Brycen Medart <brycemedart@gmail.com>
2021-09-09 17:59:20 -0700

    * Create remote variables * Reduce comments and format script

--- convert.sh
+++ convert.sh
@@ -1,141 +1,106 @@
 #!/bin/bash
 
-#Directory
-m_path="$1" 
+#File Directory Parameter
+m_path="$1"
 
-#Name
-m_name="$2" 
+#File Name Parameter
+m_name="$2"
 
-#Category
-m_cat="$3" 
+#File Category Parameter - ie (Adult Movie, Shows, etc..)
+m_cat="$3"
 
-#Full Path
-m_fpath="$4" 
+#File Full Path Parameter
+m_fpath="$4"
 
 #echo "$m_path"
 #echo "$m_name"
 #echo "$m_cat"
 #echo "$m_fpath"
 
-#Folder to save converted movies
-m_saveFolder="/media/brymed/Backup/VideoConvert/Converted"
+#Converted File Save Folder
+m_saveFolder="/home/brymed/Videos/temp"
 
+#Local Information
+rsa_keyName="piplex"
 
-transfer_file () {
-echo "transfer"
+#Remote Server Information For SFTP Transfer
+remote_ip="192.168.1.121"
+remote_username="pi"
+remote_saveFolder="/media/PIPLEX"
+
+transfer_file() {
+      #//TODO change key file to point to correct ssh key
+      scp -i ~/.ssh/id_rsa.pubfile.txt $remote_username@$remote_ip:/$remote_saveFolder/$m_cat
 }
 
-convert_file () {
+convert_file() {
 
-for i in *;
-do
+      for i in *; do
 
-#Get file extension
-file_ext="${i##*.}"
+            #GET FILE EXTENSION FROM FILE
+            file_ext="${i##*.}"
 
-#Get file name 
-file_name="${m_name%.*}"
+            #GET FILE NAME FROM FILE
+            file_name="${i%.*}"
 
-##
-#If not a directory then proceed
-##
+            # NOT EQUAL TO DIRECTORY THEN PROCEED
+            if [ ! -d "$i" ]; then
 
-if [ ! -d "$i" ] 
-then
+                  
+                  if [[ $file_ext == "mp4" ]] || [[ $file_ext == "avi" ]] || [[ $file_ext == "mkv" ]] || [[ $file_ext == "wmv" ]] || [[ $file_ext == "flv" ]] || [[ $file_ext == "mov" ]]; then
 
-##
-#If a movie file extension
-##
+                        #ASSIGN VIDEO CODECS TO VARIABLES
+                        codecV=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
+                        codecA=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
 
-if [[ $file_ext == "mp4" ]] || [[ $file_ext == "avi" ]] || [[ $file_ext == "mkv" ]] || [[ $file_ext == "wmv" ]] || [[ $file_ext == "flv" ]] || [[ $file_ext == "mov" ]] 
-then
+                        echo "$codecV"
+                        echo "$codecA"
 
-##
-#PULL VIDEO AND AUDIO CODECS FROM THE MOVIE FILE
-##
+                        #IF H264 & MP4, TRANSFER FILE WITHOUT CONVERSION
+                        if [[ "$codecV" == "h264" ]] && [[ $file_ext == "mp4" ]]; then
+                              echo "Match - h264/mp4, mv file to folder to ftp"
 
-codecV=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
-codecA=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
+                        #ELSE RECODE TO H264 & MP4
+                        else
+                              echo "Not h264 or .mp4 "
+                              #      ffmpeg -i "$i" -codec copy "${i%.*}.mp4"
+                              #     while pidof /usr/bin/ffmpeg; do sleep 10; done >/dev/null #sleep until movie is processed
+                              #    chmod 777 "${i%.*}.mp4"
+                              #   mv "${i%.*}.mp4" "$m_saveFolder"
+                              #  rm "$i"
 
-echo "$codecV"
-echo "$codecA"
+                        fi
 
-##
-#If h264 codec and mp4, transfer file without converting
-##
+                  elif [ $file_ext == "srt" ]; then
+                        echo "Exists - srt file"
 
-if [[ "$codecV" == "h264" ]] && [[ $file_ext == "mp4" ]]
-then
-echo "Match - h264/mp4, mv file to folder to ftp"
+                  #Functions - uncomment to enable
+                  #mv "$i" "$m_saveFolder/$file_name.en.srt"
 
-##
-#Else recode codec and file ext to h264/.mp4
-##
+                  else
+                  #rm "$i"
 
-else
-echo "Not h264 or .mp4 "
-#      ffmpeg -i "$i" -codec copy "${i%.*}.mp4"
-   #     while pidof /usr/bin/ffmpeg; do sleep 10; done >/dev/null #sleep until movie is processed
-    #    chmod 777 "${i%.*}.mp4"
-     #   mv "${i%.*}.mp4" "$m_saveFolder"
-      #  rm "$i"
+                  fi
+            fi
 
-fi
-  
-
-
-##       
-#If srt file change ext to .en.srt and move to converted folder
-##
-
-elif [ $file_ext == "srt" ]
-then 
-echo "Exists - srt file"
-
-#Functions - uncomment to enable
-#mv "$i" "$m_saveFolder/$file_name.en.srt"
-
-##
-#Delete any file that is not a movie file or .srt file
-##
-
-else
-rm "$i" 
-
-fi
-fi
-
-done
-
+      done
 
 }
 
-##
 #If folder for video exists then CD to directory and process files
-##
+# IF PARENT FOLDER EXISTS, CD TO DIRECTORY AND PROCESS
+if [ -d "$m_path" ]; then
+      echo "Folder exists"
+      cd "$m_path"
+      convert_file
 
-if [ -d "$m_path" ] 
-then 
-echo "Folder exists"
-cd "$m_path"
-convert_file 
-
-##
-#If no folder exists check for video files within m_fpath location
-##
-
-elif [ -f "$m_path" ] 
-then
-echo "no folder, this is file"
-cd "$m_path"
-convert_file
+# ELSE CD TO PATH AND PROCESS FILE
+elif [ -f "$m_path" ]; then
+      echo "no folder, this is file"
+      cd "$m_path"
+      convert_file
 
 fi
 
 #//TODO Remove when complete
 read -n 1 -r -s -p $'Press enter to continue...\n'
-
-
-
-
-
