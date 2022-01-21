e3b4e9e81973ce8108bdc0fad7e3bbfe243fa85a
Brycen Medart <treantlabs@gmail.com>
2022-01-21 15:56:57 -0800

    Fixed issues with formatting names * added functionality to remove characters from movie path before convert

--- convert.sh
+++ convert.sh
@@ -10,26 +10,31 @@
 ##--------------------
 
 #File Directory Parameter
-m_path="$1"
+path="$1"
 
 #File Category Parameter - ie (Adult Movie, Shows, etc..)
-m_cat="$2"
+cat="$2"
 
 #File Full Path Parameter
-m_fpath="$3"
+f_path="$3"
 
 #Array of Movie ext types
-ext_array=("avi" "flv" "mkv" "mov" "mp4" "wmv")
+ext_array=("srt" "avi" "flv" "mkv" "mov" "mp4" "wmv")
 
 ##-------------
 #Global change variables - Change to configure for your environment.
 ##--------------------
 
+#Enable file name rewrite to remove certain characters programmatically
+name_rewrite=1
+old_char=" "
+new_char="."
+
 #Remove converted local file after transfer 0=no 1=yes
 remove_file=1
 
 #Video File Save Folder, alter to save converted movies to new location.
-m_saveFolder="/home/brymed/Videos/temp"
+m_saveFolder="/home/brymed/Videos/tmp/"
 
 #SSH Key file location/name
 rsa_keyName=".ssh/id_rsa.pub"
@@ -40,92 +45,112 @@ remote_ip="192.168.1.121"
 remote_username="pi"
 remote_saveFolder="/media/PIPLEX"
 
+##
+## For all files in specified directory, rename file to remove spaces and replace with '.' character
+##
+function rename_files(){
+      
+      for f in *; do 
+            if [ ! -d "$i" ]; then
+            echo "renaming : $f to ${f//$old_char/$new_char}"
+            mv "$f" "${f//$old_char/$new_char}";
+            fi
+      done 
+}
+
 function transfer_file() {
 
       #Assign Argument to variable
-      file=$1
-      scp -i ~/$rsa_keyName "$file" $remote_username@$remote_ip:/$remote_saveFolder/$m_cat
+      i=$1
+      echo "Transferring - $i"
+      scp -i ~/$rsa_keyName "$i" $remote_username@$remote_ip:/$remote_saveFolder/$cat
 
       #Remove file after transfer if $remove_file is set
       sleep 1
       if [ $remove_file -eq 1 ]; then
-            rm "$file"
+            echo "Transfer complete - Removing file - $i"
+            rm "$i"
+            echo "Removed File - $i"
       fi
 }
 
 function convert_file() {
 
-      #Argument Passed From For Loop
-      i=$1
+#Argument Passed From For Loop
+i=$1
 
-      #GET FILE EXTENSION FROM FILE
-      file_ext="${i##*.}"
+#GET FILE EXTENSION FROM FILE
+file_ext="${i##*.}"
 
-      #GET FILE NAME FROM FILE
-      file_name="${i%.*}"
+#GET FILE NAME FROM FILE
+file_name="${i%.*}"
 
-      #If not a directory
-      if [ ! -d "$i" ]; then
-
-            # Search array for video ext
-            if [[ " ${ext_array[@]} " =~ " ${file_ext} " ]]; then
-
-                  #Assign Video/Audio codecs to variables
-                  codecV=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
-                  codecA=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
-
-                  #IF H264 & MP4, transfer without conversion
-                  if [[ "$codecV" == "h264" ]] && [[ $file_ext == "mp4" ]]; then
-                        chmod 777 "$i"
-                       
-                        mv "$i" "$m_saveFolder"
-
-                        #If transfer is enabled then transfer file
-                        if [ $enable_transfer -eq 1 ]; then
-                              transfer_file "$m_saveFolder/$i"
-                        fi
-
-                  #RECODE TO H264 & MP4
-                  else
-
-                        #Use ffmpeg to convert video, see https://ffmpeg.org/ffmpeg.html for alternate values
-                        ffmpeg -i "$i" -c:v libx264 -preset fast -crf 22 -c:a aac "${i%.*}.mp4"
-
-                        #Sleep until movie is processed !important
-                        while pidof /usr/bin/ffmpeg; do sleep 10; done >/dev/null
-
-                        chmod 777 "${i%.*}.mp4"
-                        mv "${i%.*}.mp4" "$m_saveFolder"
-                        rm "$i"
-
-                        #If transfer is enabled then transfer file
-                        if [ $enable_transfer -eq 1 ]; then
-                              transfer_file "$m_saveFolder/${i%.*}.mp4"
-                        fi
-                  fi
-
-            elif [ $file_ext == "srt" ]; then
-                  mv "$i" "$m_saveFolder/$file_name.en.srt"
-
-                  if [ $enable_transfer -eq 1 ]; then
-                        transfer_file "$m_saveFolder/$file_name.en.srt"
-                  fi
-            else
-           
-                  rm "$i"
+if [ $file_ext == "srt" ]; then
+      mv "$i" "$f_path/$file_name.en.srt"
+      rm $i
+            if [ $enable_transfer -eq 1 ]; then
+                  transfer_file "$f_path/$file_name.en.srt"
             fi
+else
+
+#Assign Video/Audio codecs to variables
+codecV=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
+codecA=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$i")
+
+      
+#RECODE TO H264 & MP4 and reduce size
+#Use ffmpeg to convert video, see https://ffmpeg.org/ffmpeg.html for alternate values
+
+echo "Converting File - $i"
+ffmpeg -i "$i" -c:v libx264 -preset medium -crf 24 -c:a aac "$file_name.mp4"
+
+#Sleep until movie is processed !important
+while pidof /usr/bin/ffmpeg; do sleep 10; done >/dev/null
+
+chmod 777 "$file_name.mp4"
+rm "$i"
+
+      #If transfer is enabled then transfer file
+      if [ $enable_transfer -eq 1 ]; then
+            transfer_file "$f_path/$file_name.mp4"
       fi
+     
+fi
 }
 
-#Loop through files in m_fpath location and send to convert_file function for processing
-cd "$m_fpath"
+#CD into directory to avoid unwanted actions
+cd $f_path
+
+#If rename is enabled, loop files and remove characters specified by above variables old_char and new_char
+if [ $name_rewrite -eq 0 ]; then
+      rename_files
+fi
+
+#Loop through files in f_path location and send to convert_file function for processing
 for i in *; do
-      convert_file "$i"
+
+ #If not a directory
+if [ ! -d "$i" ]; then
+
+#GET FILE EXTENSION FROM FILE
+file_ext="${i##*.}"
+            
+      # Search array for video ext
+      if [[ " ${ext_array[*]} " =~ " ${file_ext} " ]]; then
+            convert_file "$i"
+      else
+            #make sure it is a file
+            if [ -f $i ]; then
+                  rm $i
+                  echo "$i has been removed"
+            fi
+      fi
+fi
 done
 
-#If the movie path does not equal the save folder path delete movie folder
-if [ "$m_fpath" != "$m_saveFolder" ]; then
-      rmdir "$m_fpath"
+#If the movie path does not equal the save folder and is empty delete movie folder
+if [[ "$f_path" != "$path" ]] && [[ -d "$f_path" ]] && [[ -z "$(ls -A $f_path)" ]]; then
+      rmdir "$f_path"
 fi
 
 read -n 1 -r -s -p $'Press enter to continue...\n'
